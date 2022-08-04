2cf959e5ea07b40228aafbe169b3fbc617033e6b
khai.truong <khai.truong@trustingsocial.com>
2022-08-04 13:00:20 +0700

    Init dotfiles

---
+++ install.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+# Homebrew Script for OSX
+# To execute: save and `chmod +x ./brew-install-script.sh` then `./brew-install-script.sh`
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+brew tap homebrew/cask
+
+# Commands
+brew install docker
+brew install git curl
+brew install tmux tmuxinator
+brew install neovim
+brew install node yarn nvm
+brew install nvm
+brew install cmake
+brew install bat asdf fzf tree
+
+# Apps
+brew install --cask google-chrome
+brew install --cask ngrok
+brew install --cask slack
+brew install --cask zoom
+brew install --cask visual-studio-code
+brew install --cask iterm2
+brew install --cask mockoon
+
+# Trusting Social
+brew install --cask pritunl 
+
+# Install Oh My Zsh (https://github.com/robbyrussell/oh-my-zsh)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Install zsh-syntax-highlighting (https://github.com/zsh-users/zsh-syntax-highlighting/blob/master/INSTALL.md)
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Install zsh-autosuggestions (https://github.com/zsh-users/zsh-autosuggestions/blob/master/INSTALL.md)
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+# Install vim-plug (https://github.com/junegunn/vim-plug)
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+
+
+
+
+
+
+
+
+
+
+
+
